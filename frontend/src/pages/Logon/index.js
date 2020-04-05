3577b69d0df57a91e2ec64ad95eacdb130980a46
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-05 16:37:31 -0300

    Update (2020-04-05 16:37:31)

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -17,7 +17,7 @@ function Logon() {
         </form>
 
         <a href="/register">
-          <Flo
+          <FiLogIn />
           Não tenho cadastro</a>
       </section>
       <img src={heroesImg} alt="heroes " />
