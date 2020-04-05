fb465f03abeedd9f332f6cbc018ec0a7a68f0de5
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-05 16:13:09 -0300

    Update (2020-04-05 16:13:09)

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './style.css'
+import './style.css'
+
 function Logon() {
   return (
    <div className="logon-container">
      <section className="form">
-       
+
      </section>
    </div>
   );
