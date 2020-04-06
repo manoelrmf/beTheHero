41dfade8ac53fe2baa31ae6ab3943eaf2eb4c472
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-06 18:17:22 -0300

    Update index.js

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { FiPower, FiTrash2 } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import './style.css'
 import logoImg from '../../assets/logo.svg'
 import api from '../../services/api'
 
 function Profile() {
   const [incidents, setIncidents] = useState([])
-
+  const history = useHistory()
   const ongName = localStorage.getItem('ongName')
   const ongID = localStorage.getItem('ongId')
 
@@ -20,6 +20,26 @@ function Profile() {
       setIncidents(response.data)
     })
   }, [ongID])
+
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongID,
+        }
+      })
+
+      setIncidents(incidents.filter(incident => incident.id !== id))
+    } catch (error) {
+      alert('Erro ao deletar caso')
+    }
+  }
+
+  function handleLogout(){
+    localStorage.clear()
+    history.push("/")
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -27,7 +47,7 @@ function Profile() {
         <span>Bem vinda, {ongName}</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-        <button type="button">
+        <button type="button" onClick={handleLogout}>
           <FiPower size={18} color="#e02041" />
         </button>
       </header>
@@ -46,7 +66,7 @@ function Profile() {
             <strong>VALOR:</strong>
             <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
 
-            <button type="button">
+            <button type="button" onClick={() => handleDeleteIncident(incident.id)}>
               <FiTrash2 size={20} color="#a8a8b3" />
             </button>
           </li>
