4f61e5c8ed8a2d41fcda112956f7258bb38ba00d
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-06 16:51:13 -0300

    Update index.js

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 import './style.css'
 import heroesImg from '../../assets/heroes.png'
 import logoImg from '../../assets/logo.svg'
@@ -15,10 +16,10 @@ function Logon() {
           <input placeholder="Sua ID" />
           <button className="button" type="submit" >Entrar</button>
 
-          <a href="/register">
-          <FiLogIn size={16} color="#E02041" />
-          Não tenho cadastro
-        </a>
+          <Link className="back-link" to="/register">
+            <FiLogIn size={16} color="#E02041" />
+            Não tenho cadastro
+        </Link>
         </form>
       </section>
       <img src={heroesImg} alt="heroes " />
