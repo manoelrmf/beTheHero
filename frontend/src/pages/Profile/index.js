cee72eeb4cea94999b5dc92ba076d323b9dcf2d9
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-06 17:26:11 -0300

    Update index.js

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FiPower, FiTrash2 } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 import './style.css'
 import logoImg from '../../assets/logo.svg'
 
 function Profile() {
+  const ongName = localStorage.getItem('ongName')
+
   return (
     <div className="profile-container">
       <header>
-        <img src={logoImg} alt="Be The Hero"/>
-        <span>Bem vinda, APAD</span>      
+        <img src={logoImg} alt="Be The Hero" />
+        <span>Bem vinda, {ongName}</span>
 
-        <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>  
+        <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
         <button type="button">
           <FiPower size={18} color="#e02041" />
         </button>
