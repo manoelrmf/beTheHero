cb2682a34eac7474a26fe61b18fd23a41800c251
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-17 18:47:56 -0300

    envia incident como paramentro para página de detail

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -7,15 +7,18 @@ import api from './../../services/api'
 import { Feather } from '@expo/vector-icons'
 export default function Incidents() {
     const [incidents, setIncidents] = useState([])
+    const [total, setTotal] = useState(0)
     const navigation = useNavigation()
 
-    function navigateToDetail() {
-        navigation.navigate('Detail')
+    function navigateToDetail(incident) {
+        navigation.navigate('Detail', { incident })
     }
 
     async function loadIncidents() {
         const response = await api.get('incidents')
         setIncidents(response.data)
+        setTotal(response.headers['x-total-count'])
+
     }
 
     useEffect(() => {
@@ -28,7 +31,7 @@ export default function Incidents() {
             <View style={styles.header}>
                 <Image source={logoImg} />
                 <Text style={styles.headerText}>
-                    Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+                    Total de <Text style={styles.headerTextBold}>{total} casos</Text>.
                 </Text>
             </View>
 
@@ -58,7 +61,7 @@ export default function Incidents() {
 
                         <TouchableOpacity
                             style={styles.detailsButton}
-                            onPress={navigateToDetail}
+                            onPress={() => navigateToDetail(incident)}
                         >
                             <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
                             <Feather name="arrow-right" size={16} color="#e02041" />
