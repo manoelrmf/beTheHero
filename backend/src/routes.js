8ac03d670d934f837db901263f0c4dfb51ffc90c
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-18 09:21:43 -0300

    Valida rota de criação de ong

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { celebrate , Segments, Joi} = require('celebrate')
 
 const OngController = require('./controllers/OngController.js')
 const IncidentsController = require('./controllers/IncidentController.js')
@@ -11,7 +12,15 @@ routes.post('/sessions', SessionController.create)
 
 
 routes.get('/ongs', OngController.findAll)
-routes.post('/ongs', OngController.create)
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}),OngController.create)
 
 routes.get('/profile', ProfileController.find)
 
